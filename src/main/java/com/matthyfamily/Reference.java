d1e8952c83633a693ad39105a0137e91b5b184d7
MatthyPlayz <cjmatthy200@gmail.com>
2019-04-08 17:26:25 -0400

    Main commit
    Commits everything i've done so far before creating this repo.

---
+++ src/main/java/com/matthyfamily/Reference.java
@@ -0,0 +1,8 @@
+package com.matthyfamily;
+
+public class Reference {
+	public static final String MODID = "ue";
+	public static final String MODNAME = "Useful Emeralds";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
+}
